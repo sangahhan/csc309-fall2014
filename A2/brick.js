a58d6b8d030e03f80d83d86f69d429c83b0e0f64
Sangah <sangah93@gmail.com>
2014-10-16 08:09:40 -0400

    Changed up the bricks for fun :P

--- A2/brick.js
+++ A2/brick.js
@@ -8,9 +8,10 @@ Brick.prototype.constructor = Brick;
 
 Brick.prototype.testHit = function (ball) {
 	var x_min = this.x;
-	var x_max = x_min + this.width + ball.width;
+	var x_max = x_min + this.width;
+	var ball_centre = ball.x + (ball.width / 2);
 	var bottom_edge = this.y + this.height;
-	if ((ball.x >= x_min && ball.x <= x_max) 
+	if ((ball_centre >= x_min && ball_centre <= x_max) 
 			&& (ball.y == bottom_edge)) return true;
 	return false;
 }
