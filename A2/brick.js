7d396e449cc5b5887ed687d57110b4625124e556
Sangah <sangah93@gmail.com>
2014-10-16 11:50:53 -0400

    intermediate fix for testhit

--- A2/brick.js
+++ A2/brick.js
@@ -16,9 +16,12 @@ Brick.prototype.testHit = function (ball) {
 	var y_max = y_min + this.height;
 	var ball_top = ball.y;
 	var ball_bottom = ball_top + ball.height;
+	
+	var hitsSides = (ball_top >= y_min && ball_bottom <= y_max) 
+		&& (ball_right == x_min || ball_left == x_max);
+	var hitsTopBottom = (ball_left >= x_min && ball_right <= x_max) 
+		&& (ball_bottom == y_min || ball_top == y_max);
 
-	return (ball_centre >= x_min && ball_centre <= x_max) 
-			&& (ball.y + ball.height >= y_min 
-					&& ball.y <= y_max);
+	return (hitsSides || hitsTopBottom);
 	return false;
 }
