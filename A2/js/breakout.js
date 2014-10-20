d4c092dfbb120ea2e10ac102a26e0c203f0c1983
Binuri Walpitagamage <g3tdirty@cdf.toronto.edu>
2014-10-20 01:22:27 -0400

    Added more comments and fixed the error in changing the ball direction in the resetBoard method

--- A2/js/breakout.js
+++ A2/js/breakout.js
@@ -28,14 +28,14 @@ var BALL_R = 10;
 
 var canvas;
 var interval;
-var bricks; 
+var bricks;
 var balls = [];
 var paddle;
 var score = 0;
 var playing = false;
 var hasWon = false;
 
-var rightKeyPressed = false; 
+var rightKeyPressed = false;
 var leftKeyPressed = false;
 var smallPaddle = false;
 var currPaddleSpeed = 8;
@@ -52,9 +52,13 @@ var speedIncreaseRedRow = false;
  */
 function inheritFrom(type) {
 	function F() {}; // Dummy constructor
-	F.prototype = type; 
-	return new F(); 
+	F.prototype = type;
+	return new F();
 };
+
+/*********************************************************************
+ * 	GamePiece - Parent class of Bricks, Ball and the Paddle
+ ********************************************************************/
 
 /*
  * Any piece from the game that will appear on the canvas.
@@ -78,8 +82,12 @@ GamePiece.prototype.draw = function() {
 	ctx.fillRect(this.x, this.y, this.width, this.height);
 };
 
+/*********************************************************************
+ * 	Ball - Has the ability to move all around the canvas
+ ********************************************************************/
+
 /*
- * The game ball; takes canvas, x & y coordinates, a background colour and 
+ * The game ball; takes canvas, x & y coordinates, a background colour and
  * radius
  */
 function Ball(canvas, x, y, bkg, rad) {
@@ -90,7 +98,7 @@ function Ball(canvas, x, y, bkg, rad) {
 Ball.prototype = inheritFrom(GamePiece.prototype);
 Ball.prototype.constructor = Ball;
 
-/* 
+/*
  * Draw a ball as a circle on its canvas.
  */
 Ball.prototype.draw = function () {
@@ -105,17 +113,25 @@ Ball.prototype.draw = function () {
  * Change a ball's x & y coordinates based on its current position.
  */
 Ball.prototype.move = function () {
+	// Move horizontally by xdirection units
 	this.x += xdirection;
-	if (this.x - this.width/2 <= 0 || 
+
+	// If the right edge of the ball hits the right wall or if the
+	// left most point of the ball hits the the left ball
+	if (this.x - this.width/2 <= 0 ||
 			(this.x >= canvas.width - this.width/2)) {
 		if (this.x - this.width/2 <= 0) {
 			this.x = this.width/2;
 		} else {
 			this.x = canvas.width - this.width/2;
 		}
+		// Have to change the xdirection if it hits an edge
 		xdirection = xdirection * -1;
 	}
+
+	// Move vertically
 	this.y += ydirection;
+	// If the ball hits the top or the bottom wall
 	if (this.y -  this.width/2 <= 0 || (this.y > canvas.height )) {
 		if (this.y - this.width/2 <= 0){
 			if (!smallPaddle) {
@@ -124,14 +140,19 @@ Ball.prototype.move = function () {
 			this.y = this.width/2;
 		} else {
 			this.y = canvas.height;
-		}	
+		}
+		// change the ydirection
 		ydirection = ydirection * -1;
 	}
 };
 
+
+/*********************************************************************
+ * 	Brick - Stationary object
+ ********************************************************************/
 /*
  * A brick for the game -- takes canvas, x & y coordinates, background colour,
- * border colour width height, and score value (how many points hitting this 
+ * border colour width height, and score value (how many points hitting this
  * brick will get you).
  */
 function Brick(canvas, x, y, bkg, stroke, w, h, s) {
@@ -143,10 +164,10 @@ function Brick(canvas, x, y, bkg, stroke, w, h, s) {
 Brick.prototype = inheritFrom(GamePiece.prototype);
 Brick.prototype.constructor = Brick;
 
-/* 
+/*
  * Draw a brick as a normal gamepiece and then add a border.
  */
-Brick.prototype.draw = function(){	
+Brick.prototype.draw = function(){
 	GamePiece.prototype.draw.call(this);
 	var ctx = this.context;
 	ctx.lineWidth  = 1;
@@ -154,6 +175,9 @@ Brick.prototype.draw = function(){
 	ctx.strokeRect(this.x, this.y, this.width, this.height);
 };
 
+/*********************************************************************
+ * 	Paddle - Can move along the x coordinate
+ ********************************************************************/
 function Paddle(canvas, x, img, w, h) {
 	GamePiece.call(this, canvas, x, canvas.height - h, img, w, h);
 	this.speed = currPaddleSpeed;
@@ -168,9 +192,9 @@ Paddle.prototype.constructor = Paddle;
 Paddle.prototype.move = function(){
 	if (rightKeyPressed){
 		this.x += this.speed;
-		if (this.x + this.width > canvas.width) 
+		if (this.x + this.width > canvas.width)
 			this.x = canvas.width - this.width;
-	} 
+	}
 	if (leftKeyPressed){
 		this.x -= this.speed;
 		if (this.x < 0) this.x = 0;
@@ -186,6 +210,7 @@ Paddle.prototype.shrinkSize = function(){
 	this.width = this.width/2;
 };
 
+
 /*
  * Draw all bricks in the given list of bricks.
  */
@@ -195,7 +220,7 @@ function drawBricks(bricks) {
 	}
 };
 
-/* 
+/*
  * Generate a new list of bricks with the specified number of rows and columns.
  */
 function newBricks(rows, cols) {
@@ -204,10 +229,10 @@ function newBricks(rows, cols) {
 	var current_y = 0;
 	for (var i = BRICK_ROWS; i > 0; i--) {
 		for (var j = 0; j < cols; j++) {
-			bricks.push(new Brick(canvas, current_x, current_y, 
-						BRICK_BKG[i-1], 
-						BRICK_BKG_S[i-1], 
-						BRICK_W, BRICK_H, 
+			bricks.push(new Brick(canvas, current_x, current_y,
+						BRICK_BKG[i-1],
+						BRICK_BKG_S[i-1],
+						BRICK_W, BRICK_H,
 						BRICK_SCORES[i-1]));
 			current_x += BRICK_W;
 		}
@@ -232,7 +257,7 @@ function newBalls(num) {
 
 /*
  * Given x and y coordinates, calculate the brick to which the coordinate
- * belongs to. Return the brick's index in the list, the row and the column the 
+ * belongs to. Return the brick's index in the list, the row and the column the
  * brick as a list.
 */
 function calculateBrickLocation(x, y){
@@ -242,36 +267,38 @@ function calculateBrickLocation(x, y){
 	return [index, row, col];
 }
 
-/* 
- * Return true if the current ball hits a brick, false otherwise. 
+/*
+ * Return true if the current ball hits a brick, false otherwise.
  * Precondition: a ball exists in the global list of balls
  */
 function testHitBricks() {
-	if (!bricks.length || 
-			balls[0].y - balls[0].radius > BRICK_ROWS * BRICK_H) 
-		return false; 
+	if (!bricks.length ||
+			balls[0].y - balls[0].radius > BRICK_ROWS * BRICK_H)
+		return false;
 	var hit = false;
-	
-	var hitOrder = [[balls[0].x, balls[0].y - balls[0].radius], // bottom 
-		[balls[0].x + balls[0].radius, balls[0].y], // left 
+
+	var hitOrder = [[balls[0].x, balls[0].y - balls[0].radius], // bottom
+		[balls[0].x + balls[0].radius, balls[0].y], // left
 		[balls[0].x, balls[0].y + balls[0].radius], // top
 		[balls[0].x - balls[0].radius, balls[0].y]]; // right
-	
+
 	var index; var row; var col;
 	for (var i = 0; i < 4; i++){
 		result = calculateBrickLocation(hitOrder[i][0], hitOrder[i][1]);
 		index = result[0];
 		row = result[1];
 		col = result[2];
-		
-		if (index >= 0 && row < BRICK_ROWS && col < BRICK_COLS && 
+
+		if (index >= 0 && row < BRICK_ROWS && col < BRICK_COLS &&
 				bricks[index]){
 			hit = true;
 			if (i == 0){
 				// A hit from the bottom
-				balls[0].y = bricks[index].y + 
+				balls[0].y = bricks[index].y +
 					bricks[index].height + balls[0].radius;
 				ydirection *= -1;
+				var rand = Math.floor((Math.random() * 10) + 1);
+				if (rand > 5) xdirection *= -1;
 			} else if (i == 1){
 				// A hit from the left
 				balls[0].x = bricks[index].x -  balls[0].radius;
@@ -280,73 +307,83 @@ function testHitBricks() {
 				//A hit from the top
 				ydirection *= -1;
 				balls[0].y = bricks[index].y -  balls[0].radius;
-			} else {			
+			} else {
 				// A hit from the right
-				balls[0].x = bricks[index].x + 
+				balls[0].x = bricks[index].x +
 					bricks[index].width + balls[0].radius;
 				xdirection *= -1;
 			}
 
 			score += bricks[index].score;
 			bricks[index] = 0;
-	
+
 			// Increase ball speed after a certain number of hits
 			numHits += 1;
 			if (numHits == 4 || numHits == 12){
 				increaseBallSpeed();
 			}
-		
-			if ((row == 2 || row == 3) && 
+
+			// Increase the speed if it hits the orange row and if the
+			// speed has not already been increased
+			if ((row == 2 || row == 3) &&
 					(!speedIncreaseOrangeRow)) {
 				speedIncreaseOrangeRow = true;
 				increaseBallSpeed();
 			}
+
+			// Increase the speed when hitting the red row for the first time
 			if ((row == 0 || row == 1) && (!speedIncreaseRedRow)) {
 				speedIncreaseRedRow = true;
 				increaseBallSpeed();
 			}
 		}
-		
+
 		if (hit) break;
-	}            
+	}
 	return hit;
 };
 /*
- * Return true if the current ball hits the paddle, false otherwise. 
+ * Return true if the current ball hits the paddle, false otherwise.
  */
 function testHitPaddle() {
-	if (!balls.length) return false;	
+	if (!balls.length) return false;
 	var x_min = paddle.x;
 	var x_max = paddle.x + paddle.width;
 	var y = paddle.y;
 	var ball_bottom = balls[0].y + balls[0].radius;
-	return ydirection > 0 && balls[0].x >= x_min && 
+	return ydirection > 0 && balls[0].x >= x_min &&
 		balls[0].x <= x_max && ball_bottom >= y;
 };
 
 /*
- * Toggle being able to move the paddle if left/right arrow keys are pressed. 
+ * Toggle being able to move the paddle if left/right arrow keys are pressed.
  * Stop / start the game if the enter key is pressed.
  */
 function movePaddle(evt) {
 	if (evt.keyCode == 39) rightKeyPressed = true;
 	else if (evt.keyCode == 37) leftKeyPressed = true;
-	else if (evt.keyCode == 13) {
+	else if (evt.keyCode == 13) { // <Enter> pressed
 		if (balls.length && !hasWon) {
+			// There are still balls left and the person has not won.
+			// If the user is playing and presses <Enter>, then it is
+			// to pause the game, otherwise, it is to resume from pause.
 			if (playing) gameStop();
-			else { 
+			else {
 				gameStart();
 				ballsSpan.html("Balls left: " + balls.length);
 			}
 		} else {
+			// If there are no more balls remaining or the person has won
+			// Then the user will press enter to restart the whole game
+			gameStop();
 			hasWon = false;
 			// back to beginning
 			score = 0;
 			// reset the balls before we try to draw them
 			balls = newBalls(3);
 			resetBoard(1);
-			gameStop();
 			drawAll(0);
+
 			ballsSpan.html("Press ENTER to start/pause the game.");
 			scoreSpan.html("Score: " + score);
 		}
@@ -355,7 +392,7 @@ function movePaddle(evt) {
 };
 
 /*
- * Toggle being able to move the paddle if the left/right arrow keys are 
+ * Toggle being able to move the paddle if the left/right arrow keys are
  * released.
  */
 function stopMovingPaddle(evt) {
@@ -373,58 +410,61 @@ function shrinkPaddle(){
 }
 
 /*
- * Increase ball speed.
+ * Increase ball speed. Since we need the paddle to move
+ * as fast as the ball, we increase the paddle speed as well.
  */
 function increaseBallSpeed(){
 	xdirection > 0? xdirection += 2: xdirection -= 2;
 	ydirection > 0? ydirection += 1: ydirection -= 1;
-	
-	// When the paddle is small, need to move faster
-	smallPaddle? paddle.speed += 5: paddle.speed += 3;
+
+	paddle.speed += 5;
 	currPaddleSpeed = paddle.speed;
 }
 
-/* 
+/*
  * Draw bricks, paddle and current ball on the board.
+ * Move the ball to the next location if required.
  * Precondition: balls exist in the global list of balls.
  */
 function drawAll(moveBall) {
 	canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
 	drawBricks(bricks);
 	balls[0].draw();
-	if (moveBall) balls[0].move();
 	paddle.draw();
+
+	if (moveBall) balls[0].move();
 };
 
-/* 
- * Clear the board, reset all values and draw again.
+/*
+ * Clear the board, reset all values.
  * Takes a boolean that will determine whether or not to reset the balls.
  */
-function resetBoard(resetBalls) {	
+function resetBoard(resetBalls) {
 	numHits = 0;
 	speedIncreaseOrangeRow = false;
 	speedIncreaseRedRow = false;
-	
+
 	bricks = newBricks(BRICK_ROWS, BRICK_COLS);
-	paddle = new Paddle(canvas, (canvas.width / 2) - (PADDLE_W/ 2), 
+	paddle = new Paddle(canvas, (canvas.width / 2) - (PADDLE_W/ 2),
 			PADDLE_I, PADDLE_W, PADDLE_H);
-	if (resetBalls) balls = newBalls(3);	
-	/* 
-	 * If the game is reset to the very beginning, then reset the speed of 
-	 * the ball and the paddle. Otherwise, only change the direction of the 
+
+	if (resetBalls) balls = newBalls(3);
+	/*
+	 * If the game is reset to the very beginning, then reset the speed of
+	 * the ball and the paddle. Otherwise, only change the direction of the
 	 * ball.
 	 */
 	smallPaddle = false;
-	
+
 	if (score < LEVEL_SCORE && numHits < 4){
 		xdirection = -4;
 		ydirection = -8;
 		currPaddleSpeed = 8;
 		paddle.speed = currPaddleSpeed;
 	} else {
-		if (ydirection > 0) direction *= -1;
+		if (ydirection > 0) ydirection *= -1;
 	}
-	
+
 };
 
 /*
@@ -432,22 +472,37 @@ function resetBoard(resetBalls) {
  */
 function levelCheck() {
 	var sum; // will either be 0 or a string
-	for (var i = 0; i < bricks.length; i++) { sum += bricks[i];} 	
+	for (var i = 0; i < bricks.length; i++) { sum += bricks[i];}
 	if (!sum) {
 		if (score == LEVEL_SCORE) {
+			// Stop the game since we need to start the second level
+			gameStop();
+
+			// Reset the board leaving the current balls
 			resetBoard(0);
+
+			// Place the current ball on the paddle
 			var x = (canvas.width / 2);
 			var y = canvas.height - PADDLE_H - BALL_R;
 			balls[0].x = x;
 			balls[0].y = y;
-			gameStop();
+
+			// draw all without moving the ball because we want it to
+			// start moving after the user presses enter.
 			drawAll(0);
 			ballsSpan.html("LEVEL 2");
 			return true;
 		} else if (score == LEVEL_SCORE * 2) {
-			hasWon = true;
+			// Stop because the player has won
 			gameStop();
+
+			// Draw a final time to make the last brick disappear
+			drawAll(0);
+
+			hasWon = true;
 			score = 0;
+
+			// Reset everything including the balls
 			resetBoard(1);
 			scoreSpan.html("WINNER");
 			ballsSpan.html("Press ENTER to play again.");
@@ -461,19 +516,19 @@ function levelCheck() {
  * Take appropriate action if player has lost a ball.
  */
 function onLose(){
+	gameStop();
 	balls.shift();
 	if (balls.length) {
-		// move the ball to center
+		// move the paddle to center
 		paddle.x = (canvas.width / 2) - (paddle.width/ 2);
 		ballsSpan.html("Press ENTER to continue.");
 	} else {
 		scoreSpan.html("GAME OVER");
 		ballsSpan.html("Press ENTER to play again.");
 	}
-	gameStop();
 };
 
-/* 
+/*
  * Stop animation & all movement.
  */
 function gameStop() {
@@ -495,8 +550,10 @@ function gameStart() {
 function gameRun() {
 	if (playing) {
 		if (testHitBricks()) scoreSpan.html("Score: " + score);
+		// Check if the user hits the paddle. if he does, then
+		// make sure the ball only goes upwards.
 		if (!testHitPaddle()) {
-			if (balls[0].y >= (canvas.height - paddle.height)) {
+			if (balls[0].y - balls[0].radius > (canvas.height - paddle.height)) {
 				onLose();
 				/*
 				 * so we don't try to draw everything if there
@@ -506,9 +563,12 @@ function gameRun() {
 			}
 		} else {
 			if (balls.length) {
-				ydirection *= -1;
+				if (ydirection > 0) ydirection *= -1;
 			}
 		}
+
+		// Check to see if a user has reached the next level or won.
+		// If they haven't, then draw the game pieces and move the ball.
 		if (!levelCheck() && balls.length) drawAll(1);
 	}
 };
@@ -516,7 +576,7 @@ function gameRun() {
 $(function() {
 	scoreSpan = $("#score");
 	scoreSpan.html("Score: " + score);
-		
+
 	ballsSpan = $("#balls");
 
 	canvas = $("#game-window")[0];
@@ -526,4 +586,3 @@ $(function() {
 	$(document).keyup(stopMovingPaddle);
 	ballsSpan.html("Press ENTER to start/pause the game.");
 });
-
