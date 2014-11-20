e611176c0ebff63db594e7b4279f711a3c0d9f70
Sangah <sangah93@gmail.com>
2014-11-20 13:32:35 -0500

    autoload

--- A3/estore/application/config/autoload.php
+++ A3/estore/application/config/autoload.php
@@ -53,7 +53,7 @@ $autoload['packages'] = array();
 */
 
 //$autoload['libraries'] = array();
-$autoload['libraries'] = array('database','session','email');
+$autoload['libraries'] = array('database','session');
 
 
 /*
