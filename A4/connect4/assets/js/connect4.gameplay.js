57e88fc033e1bf9dbd76f95896e9def3d2fa78c3
Sangah Han <sangah93@gmail.com>
2014-12-07 00:27:53 -0500

    added more hypothetical backend calls

--- A4/connect4/assets/js/connect4.gameplay.js
+++ A4/connect4/assets/js/connect4.gameplay.js
@@ -1,37 +1,18 @@
-function updateBoard(row) {
-    //  TODO: change this to drop the piece in the board based on calls to backend
-        var rowIndex = row.attr('data-index');
-        // grab cells st col == data index as the recently clicked cell
-        var col = $('td[data-index$="-'+rowIndex+'"]');    
-        var topPiece = col.first();
-        var bottomPiece = col.last();
-
-        if(topPiece.hasClass('yellow-piece') || topPiece.hasClass('red-piece')){
-            alert("This column is full!");
-        } else if (!bottomPiece.hasClass('yellow-piece') && !bottomPiece.hasClass('red-piece')){           
-                bottomPiece.addClass('red-piece');    
-                newItem=bottomPiece;                
-        } else {
-            addPiece(col);        
-        }
-
-}
-
-
 $.fn.reverse = [].reverse;
 
-function addPiece(col){    
-    var colReversed = col.reverse();
-    
-    colReversed.each(function(){
-        if(!$(this).hasClass('red-piece') && !$(this).hasClass('yellow-piece')){        
-                $(this).addClass('red-piece');
-                newItem=$(this);                
-                return false;                    
-        }        
-    });
+    function addPiece(col, pieceClass){    
+        var colReversed = col.reverse();
+        
+        colReversed.each(function(){
+            if(!$(this).hasClass('red-piece') && !$(this).hasClass('yellow-piece')){        
+                    $(this).addClass(pieceClass);               
+                    return false;                    
+            }        
+        });
 
-}
+    }
+
+
 
 function renderBoard(div_selector, board) {
 
